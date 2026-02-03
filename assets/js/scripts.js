Massi-X/massi-x.github.io

commit 4c2cabdcc2c10a54cb893e3d3db7e9f5515d716c

Fix Safari Navigate API + iPad link highlight - Safari does not correctly handle border-radius and transform, switched to clip-path - Correctly calculate transititonend for new page load in Navigate code - Fix glitch highlighting links and buttons on iPad

_includes/head.html
@@ -21,7 +21,7 @@
 	<link rel="preload" as="style" onload="this.rel='stylesheet'" href="/public/assets/css/cookieconsent.css">
 	<link rel="preload" as="style" onload="this.rel='stylesheet'" href="/public/assets/css/cookieconsent_extra.css?v=1.0.5">
 
-	<link rel="stylesheet" href="/assets/css/style.css?v=1.7.1">
+	<link rel="stylesheet" href="/assets/css/style.css?v=1.7.2">
 	<link rel="preload" as="style" onload="this.rel='stylesheet'" href="/assets/fontawesome/css/fontawesome.min.css">
 	<link rel="preload" as="style" onload="this.rel='stylesheet'" href="/assets/fontawesome/css/brands.min.css">
 	<link rel="preload" as="style" onload="this.rel='stylesheet'" href="/assets/fontawesome/css/solid.min.css">
@@ -35,7 +35,7 @@
 	<script defer src="/assets/js/sweetalert2.min.js"></script>
 	<!-- temporarily disabled - <script defer src="/assets/js/iosPWASplash.js"></script> thanks to https://github.com/avadhesh18/iosPWASplash -->
 	<script defer src="/assets/js/scroll-lock.min.js"></script>
-	<script defer src="/assets/js/scripts.js?v=1.7.1"></script>
+	<script defer src="/assets/js/scripts.js?v=1.7.2"></script>
 	<script async type="text/plain" data-category="analytics"
 		data-src="https://www.googletagmanager.com/gtag/js?id=G-H8RP322KHJ"></script>
 

assets/js/scripts.js
@@ -321,10 +321,19 @@ if ('navigation' in window) {
 		//emulate a scrolling to the top on the navbar (this is fake! the page is stuck to make a better animation)
 		window.header.classList.remove('shrinked');
 
+		//calculate the longest duration on the animation target.
+		//In case of multiple transitions on the target, we should wait for the longest one
+		let duration = getDuration(animationTarget);
+
 		//animate the main loading view. This must be kept synced on the longer animation (currently the circle/slide)
 		//it is not necessary here to use setTimeout to prevent bug (user navigating while resizing) beacuse to do this, the user needs to be an acrobat!
-		animationTarget.addEventListener('transitionend', () => {
-			if (window.loadProcessing) //do not continue if loadPage is already processing the request or we will overwrite something!
+		animationTarget.addEventListener('transitionend', event => {
+			//wait for the longest duration before triggering the load (in case the next page loads very fast)
+			if(duration - (event.elapsedTime * 1000) > 50)
+				return;
+
+			//do not continue if loadPage is already processing the request or we will overwrite something!
+			if (window.loadProcessing)
 				return;
 
 			if (navigationType == 'push') {
@@ -342,7 +351,7 @@ if ('navigation' in window) {
 
 			animated = true;
 			loadPage();
-		}, { once: true });
+		}); //do not use once here!
 
 		//here we load the new content
 		intercept({ scroll: 'manual' }, loadPage);

_sass/common.scss
@@ -136,17 +136,6 @@
 	outline: 2px solid var(--outline) !important;
 	scroll-margin-top: calc(var(--navbar-shrinked) + 20px);
 	scroll-margin-bottom: 20px;
-	box-shadow: none !important;
-}
-
-@mixin link-animate($important: false) {
-	transition: box-shadow 0.1s ease if($important, !important, null);
-}
-
-@mixin link-hoverable($important: false) {
-	border-radius: 0.1em if($important, !important, null);
-	box-shadow: 0 0 0 1px var(--link-highlight),
-		0 0 0 100vh var(--link-highlight) inset if($important, !important, null);
 }
 
 %cc-text {

assets/css/style.scss
@@ -185,7 +185,7 @@ p {
 	width: 1px;
 	height: 1px;
 	background: var(--circle);
-	border-radius: 50%;
+	clip-path: circle(50%);
 	transform: scale(1);
 	transition: transform var(--circle-transform-timing)
 			cubic-bezier(0.25, 0.46, 0.45, 0.94),
@@ -385,9 +385,21 @@ h1.heading {
 /********************************************************************************
 * link styles (elements without any class -or- with specific "hoverable" class) *
 *********************************************************************************/
-a:not([class]),
-a.hoverable {
-	@include link-animate;
+a {
+	position: relative;
+}
+
+a:not([class])::after,
+a.hoverable::after {
+	content: "";
+	position: absolute;
+	height: 100%;
+	width: 100%;
+	top: 0;
+	left: 0;
+	background: var(--link-highlight);
+	transition: opacity 0.1s ease;
+	opacity: 0;
 }
 
 a:not([class]):focus-visible,
@@ -395,10 +407,19 @@ a.hoverable:focus-visible {
 	@extend %link-focusable;
 }
 
+@mixin link-hoverable() {
+	opacity: 1;
+}
+
 @mixin link-hover($hover_or_active) {
+	a:not([class]):#{$hover_or_active}::after,
+	a.hoverable:#{$hover_or_active}::after {
+		@include link-hoverable;
+	}
+
 	a:not([class]):#{$hover_or_active},
 	a.hoverable:#{$hover_or_active} {
-		@include link-hoverable;
+		outline: 2px solid var(--link-highlight);
 	}
 }
 
@@ -601,29 +622,6 @@ ul.prettier li::before {
 	html.swal2-shown {
 		overflow-y: auto;
 	}
-}
-
-/********************************************************************************
-************************* CC specific to massi-x domain *************************
-/********************************************************************************/
-.cc-link {
-	@include link-animate(true);
-	color: var(--text) !important;
-	background-size: 0 0 !important;
-}
-
-@mixin cc-link-hover($hover_or_active) {
-	.cc-link:#{$hover_or_active} {
-		@include link-hoverable(true);
-	}
-}
-
-@media (hover: hover) {
-	@include cc-link-hover("hover");
-}
-
-@media (hover: none) {
-	@include cc-link-hover("active");
 }
 
 /********************************************************************************
@@ -1662,19 +1660,10 @@ html.swal2-height-auto {
 	padding-right: 2px;
 }
 
-@mixin blog_card-hover($hover_or_active) {
-	#blog .blog-card:#{$hover_or_active} {
-		border-radius: 0;
-		box-shadow: 0 0 0 100vh var(--link-highlight) inset;
-	}
-}
-
-@media (hover: hover) {
-	@include blog_card-hover("hover");
-}
-
-@media (hover: none) {
-	@include blog_card-hover("active");
+#blog .blog-card:focus-visible,
+#blog .blog-card:focus-visible::after {
+	outline: none !important; //override default oultine implementation
+	@include link-hoverable;
 }
 
 @media only screen and (max-width: 600px) {

commit 7eada50e28ff60895d4361ac35c30318fd46a6c7

Fixed bug in traverse navigation introduced with commit 4c2cabd

_includes/head.html
@@ -35,7 +35,7 @@
 	<script defer src="/assets/js/sweetalert2.min.js"></script>
 	<!-- temporarily disabled - <script defer src="/assets/js/iosPWASplash.js"></script> thanks to https://github.com/avadhesh18/iosPWASplash -->
 	<script defer src="/assets/js/scroll-lock.min.js"></script>
-	<script defer src="/assets/js/scripts.js?v=1.7.2"></script>
+	<script defer src="/assets/js/scripts.js?v=1.7.3"></script>
 	<script async type="text/plain" data-category="analytics"
 		data-src="https://www.googletagmanager.com/gtag/js?id=G-H8RP322KHJ"></script>
 

assets/js/scripts.js
@@ -161,8 +161,8 @@ if ('navigation' in window) {
 			return;
 		}
 
-		let newPageContent;
-		let animationTarget;
+		let newPageContent = null;
+		let animationTarget = null;
 		let animated = false;
 		let newIndex = navigateEvent.destination.index;
 		let newURL = new URL(navigateEvent.destination.url);
@@ -327,10 +327,13 @@ if ('navigation' in window) {
 
 		//animate the main loading view. This must be kept synced on the longer animation (currently the circle/slide)
 		//it is not necessary here to use setTimeout to prevent bug (user navigating while resizing) beacuse to do this, the user needs to be an acrobat!
-		animationTarget.addEventListener('transitionend', event => {
-			//wait for the longest duration before triggering the load (in case the next page loads very fast)
-			if(duration - (event.elapsedTime * 1000) > 50)
+		animationTarget.addEventListener('transitionend', function handler(event) {
+			//wait for the longest duration before triggering the load
+			if (duration - (event.elapsedTime * 1000) > 50)
 				return;
+
+			//must remove listener manually as once cannot be used!
+			this.removeEventListener('transitionend', handler);
 
 			//do not continue if loadPage is already processing the request or we will overwrite something!
 			if (window.loadProcessing)
@@ -351,11 +354,10 @@ if ('navigation' in window) {
 
 			animated = true;
 			loadPage();
-		}); //do not use once here!
+		}, { passive: true }); //do not use once here!
 
 		//here we load the new content
 		intercept({ scroll: 'manual' }, loadPage);
-
 
 		//-----------------------------------------//
 		//------------- inner methods -------------//